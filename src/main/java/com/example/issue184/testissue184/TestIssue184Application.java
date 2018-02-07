68fb571c3d4be80d721a1f18b6b67d709ea01418
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-02-07 14:15:11 +0100

    Initial import

---
+++ src/main/java/com/example/issue184/testissue184/TestIssue184Application.java
@@ -0,0 +1,12 @@
+package com.example.issue184.testissue184;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIssue184Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestIssue184Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/issue184/testissue184/TestIssue184ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.issue184.testissue184;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestIssue184ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
